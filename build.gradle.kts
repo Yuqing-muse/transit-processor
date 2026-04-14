b5832fc86b072751d32fd64c1c11bfc4282350ec
Qing_ <zys91011@outlook.com>
2026-04-14 21:24:40 +0700

    initialise Gradle project

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    java
+    application
+}
+
+group = "com.littlepay"
+version = "1.0.0"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+application {
+    mainClass.set("com.littlepay.Main")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("com.opencsv:opencsv:5.9")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
+    testImplementation("org.assertj:assertj-core:3.25.3")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}

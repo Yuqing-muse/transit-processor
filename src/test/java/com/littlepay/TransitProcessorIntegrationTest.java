60de4ef864f39ef073d356ee868f9eae6028cc41
Qing_ <zys91011@outlook.com>
2026-04-15 09:53:58 +0700

    add mock file

--- src/test/java/com/littlepay/TransitProcessorIntegrationTest.java
+++ src/test/java/com/littlepay/TransitProcessorIntegrationTest.java
@@ -17,14 +17,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class TransitProcessorIntegrationTest {
 
-    private static final String EXAMPLE_TAPS =
-            "ID, DateTimeUTC, TapType, StopId, CompanyId, BusID, PAN\n" +
-            "1, 22-01-2023 13:00:00, ON, Stop1, Company1, Bus37, 5454545454545454\n" +
-            "2, 22-01-2023 13:05:00, OFF, Stop2, Company1, Bus37, 5454545454545454\n" +
-            "3, 22-01-2023 09:20:00, ON, Stop3, Company1, Bus36, 4444333322221111\n" +
-            "4, 23-01-2023 08:00:00, ON, Stop1, Company1, Bus37, 4444333322221111\n" +
-            "5, 23-01-2023 08:02:00, OFF, Stop1, Company1, Bus37, 4444333322221111\n" +
-            "6, 24-01-2023 16:30:00, OFF, Stop2, Company1, Bus37, 5454545454545454\n";
+    private static final String EXAMPLE_TAPS = """
+            ID, DateTimeUTC, TapType, StopId, CompanyId, BusID, PAN
+            1, 22-01-2023 13:00:00, ON, Stop1, Company1, Bus37, 5454545454545454
+            2, 22-01-2023 13:05:00, OFF, Stop2, Company1, Bus37, 5454545454545454
+            3, 22-01-2023 09:20:00, ON, Stop3, Company1, Bus36, 4444333322221111
+            4, 23-01-2023 08:00:00, ON, Stop1, Company1, Bus37, 4444333322221111
+            5, 23-01-2023 08:02:00, OFF, Stop1, Company1, Bus37, 4444333322221111
+            6, 24-01-2023 16:30:00, OFF, Stop2, Company1, Bus37, 5454545454545454
+            """;
 
     @Test
     void shouldBuildThreeTripsFromExampleInput() throws Exception {
@@ -39,7 +40,7 @@ class TransitProcessorIntegrationTest {
 
     @Test
     void shouldProcessTap1And2AsCompletedTrip() throws Exception {
-        List<Trip> trips = process(EXAMPLE_TAPS);
+        List<Trip> trips = process();
 
         Trip completed = trips.stream()
                 .filter(t -> t.pan().equals("5454545454545454") && t.status() == TripStatus.COMPLETED)
@@ -53,7 +54,7 @@ class TransitProcessorIntegrationTest {
 
     @Test
     void shouldProcessTap3AsIncompleteTrip() throws Exception {
-        List<Trip> trips = process(EXAMPLE_TAPS);
+        List<Trip> trips = process();
 
         Trip incomplete = trips.stream()
                 .filter(t -> t.pan().equals("4444333322221111") && t.status() == TripStatus.INCOMPLETE)
@@ -66,7 +67,7 @@ class TransitProcessorIntegrationTest {
 
     @Test
     void shouldProcessTap4And5AsCancelledTrip() throws Exception {
-        List<Trip> trips = process(EXAMPLE_TAPS);
+        List<Trip> trips = process();
 
         Trip cancelled = trips.stream()
                 .filter(t -> t.pan().equals("4444333322221111") && t.status() == TripStatus.CANCELLED)
@@ -79,7 +80,7 @@ class TransitProcessorIntegrationTest {
 
     @Test
     void shouldIgnoreOrphanedTap6() throws Exception {
-        List<Trip> trips = process(EXAMPLE_TAPS);
+        List<Trip> trips = process();
 
         // PAN 5454545454545454 has only one trip (COMPLETED from taps 1&2); tap 6 is ignored
         long countForPan = trips.stream()
@@ -90,7 +91,7 @@ class TransitProcessorIntegrationTest {
 
     @Test
     void shouldWriteOutputCsvWithCorrectHeader() throws Exception {
-        List<Trip> trips = process(EXAMPLE_TAPS);
+        List<Trip> trips = process();
         StringWriter out = new StringWriter();
         new TripCsvWriter().write(out, trips);
 
@@ -98,8 +99,8 @@ class TransitProcessorIntegrationTest {
                 "Started,Finished,DurationSecs,FromStopId,ToStopId,ChargeAmount,CompanyId,BusID,PAN,Status");
     }
 
-    private List<Trip> process(String csv) throws Exception {
+    private List<Trip> process() throws Exception {
         return new TripProcessor(new FareCalculator(FareCalculator.DEFAULT_RULES))
-                .process(new TapCsvReader().read(new StringReader(csv)));
+                .process(new TapCsvReader().read(new StringReader(EXAMPLE_TAPS)));
     }
 }
